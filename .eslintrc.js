930eca711bfac6747f8b57db53e2ab8abe2790f2
Vianney Ruhlmann <ruhlmannvianney@gmail.com>
2022-05-17 23:22:52 +0200

    add eslint typescript config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,4 +10,21 @@ module.exports = {
     "prettier",
     "plugin:@typescript-eslint/recommended",
   ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+  overrides: [
+    {
+      files: ["*.ts"],
+      processor: "@graphql-eslint/graphql",
+    },
+    {
+      files: ["*.graphql"],
+      parser: "@graphql-eslint/eslint-plugin",
+      plugins: ["@graphql-eslint"],
+      rules: {
+        "@graphql-eslint/known-type-names": "error",
+      },
+    },
+  ],
 };
