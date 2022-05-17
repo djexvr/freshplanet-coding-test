bf24b71066f4ea1853c16f1a5fdbb42af176c135
Vianney Ruhlmann <ruhlmannvianney@gmail.com>
2022-05-17 23:25:25 +0200

    add index to run the server

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import express from "express";
+import http from "http";
+import { DocumentNode } from "graphql";
+import { IResolvers } from "@graphql-tools/utils";
+import schema from "./schema";
+import resolvers from "./resolvers";
+
+async function startApolloServer(
+  typeDefs: DocumentNode,
+  resolvers: IResolvers
+) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    context: ({ req }) => ({ userId: req.headers["x-user"] }),
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+
+startApolloServer(schema, resolvers);
